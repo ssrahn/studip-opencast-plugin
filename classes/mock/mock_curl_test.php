5a914be11fb42cf224046754118207908ca027ef
jleissner <jleissner@uos.de>
2018-09-20 14:48:51 +0200

    updated test

--- classes/mock/mock_curl_test.php
+++ classes/mock/mock_curl_test.php
@@ -10,20 +10,32 @@ require_once '../OCcURL.php';
 require_once 'MockcURL.php';
 require_once 'MockcURLRequestResponse.php';
 
-
+/**
+ * GENERATING A FAKE RESPONSE
+ */
 $responses[] = new MockcURLRequestResponse(
     'foo.de/bar.php',
     404,
     '404 not found',
-    1,
-    'test'
+    3,
+    'custom error message test (real error in "number_interpreted"!'
 );
+/**
+ * RESTRICT OPTION(S)
+ */
 $restricted_fields = [CURLOPT_RETURNTRANSFER];
 
 
+/**
+ * GENERATE AS USUAL
+ */
 $curl = new MockcURL($restricted_fields,$responses);
 $curl->set_url('foo.de/bar.php');
 $curl->set_option(CURLOPT_RETURNTRANSFER, true);
 
+/**
+ * TEST SOME RESULTS
+ */
 var_dump($curl->execute());
+var_dump($curl->last_request_http_code());
 var_dump($curl->get_error_list());
