13b2af8ff4d1c61e3242eb4b27a24223767bb2d8
jleissner <jleissner@uos.de>
2018-09-21 14:10:09 +0200

    optimized setup process in one test

--- testing/OCRestClient/OCRestClientTest.php
+++ testing/OCRestClient/OCRestClientTest.php
@@ -22,65 +22,50 @@ class OCRestClientTest extends TestCase
 
     protected function setUp()
     {
-        class_alias('MockcURL','OCcURL');
-    }
-
-    public function testGetJSON()
-    {
-        $client = new OCRestClient([
+        class_alias('MockcURL', 'OCcURL');
+        $this->client = new OCRestClient([
             'service_url'      => 'foo.bar/',
             'service_user'     => 'test',
             'service_password' => 'test',
             'service_version'  => 1
         ]);
+    }
 
-        $client->ochandler->set_response(new MockcURLRequestResponse(
+    public function testGetJSON()
+    {
+        $this->client->ochandler->set_response(new MockcURLRequestResponse(
             'foo.bar/test',
             200,
             json_encode(['worked' => true])
         ));
 
-        $response = $client->getJSON('test');
+        $response = $this->client->getJSON('test');
 
         $this->assertTrue($response->worked);
     }
 
     public function testGetURL()
     {
-        $client = new OCRestClient([
-            'service_url'      => 'foo.bar/',
-            'service_user'     => 'test',
-            'service_password' => 'test',
-            'service_version'  => 1
-        ]);
-
-        $client->ochandler->set_response(new MockcURLRequestResponse(
+        $this->client->ochandler->set_response(new MockcURLRequestResponse(
             'foo.bar/test',
             200,
             'worked!'
         ));
 
-        $response = $client->getURL('test');
+        $response = $this->client->getURL('test');
 
         $this->assertTrue($response == 'worked!');
     }
 
     public function testGetXML()
     {
-        $client = new OCRestClient([
-            'service_url'      => 'foo.bar/',
-            'service_user'     => 'test',
-            'service_password' => 'test',
-            'service_version'  => 1
-        ]);
-
-        $client->ochandler->set_response(new MockcURLRequestResponse(
+        $this->client->ochandler->set_response(new MockcURLRequestResponse(
             'foo.bar/test',
             200,
             '<worked>true</worked>'
         ));
 
-        $response = $client->getXML('test');
+        $response = $this->client->getXML('test');
 
         $this->assertTrue($response == '<worked>true</worked>');
     }
