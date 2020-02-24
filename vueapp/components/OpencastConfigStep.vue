1dc95ad7bc45d42c39fa489d23f70542c0d379eb
tgloeggl <tgloeggl@uos.de>
2020-02-24 17:08:30 +0100

    Add step visualisation for config wizard

--- vueapp/components/OpencastConfigStep.vue
+++ vueapp/components/OpencastConfigStep.vue
@@ -1,6 +1,14 @@
 <template>
-    <div>
-        {{ step }} / {{ steps }}
+    <div class="steps">
+        <span v-for="index in steps">
+            <span class="step" :class="{
+                'active': index == step
+            }">
+                {{ index }}
+            </span>
+
+            <span class="spacer" v-if="index < steps"></span>
+        </span>
     </div>
 </template>
 
